862934148e9f850c9b7d5c87252f238c0269f5fd
valar <68338647+76616c6172@users.noreply.github.com>
2021-12-10 16:49:08 -0800

    VIP: AoC day3 storing boards properly in bbd slice

---
+++ adventOfCode2021/day4/README.md
@@ -0,0 +1,6 @@
+# To do:
+read input from file            DONE
+get winning numbers             DONE
+get number of boards            DONE
+create 3 dimensional slice      DONE
+store boards in the slice       VIP
--- adventOfCode2021/day4/bingo.go
+++ adventOfCode2021/day4/bingo.go
@@ -3,22 +3,17 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	//TODO:
-	//get number of boards n
-	//read each board and store it in bingo_board_data
-	//return winning numbers DONE.
-	//create 3 dimensional slice bingo_board_data[n][x][y] DONE.
 
 	//get the input from file
-	winning_nums := input_handler("example")
+	winning_nums, board_data, BOARD_AMOUNT := input_handler("example")
 
 	//bingo board data
-	//bbd[board_number][y][x]
-	BOARD_AMOUNT := 2
+	//bbd[board_index][y][x]
 	bbd := make([][][]int, BOARD_AMOUNT)
 	for a := range bbd {
 		bbd[a] = make([][]int, 5)
@@ -27,18 +22,41 @@ func main() {
 		}
 
 	}
-	//test putting in this board
-	//0 0 0 0 0
-	//0 9 0 0 0
-	//0 0 0 0 0
-	//0 0 0 0 7
-	//0 0 0 0 0
-	bbd[0][1][1] = 9
-	bbd[0][3][4] = 7
 
+	/*
+		for i, v := range board_data {
+			fmt.Println(i, v)
+		}
+	*/
+
+	//Wonderful we have a slice of rows but the fucking whitespace man
+	board_index := 0
+	inside := true
+	y := 0
+	count := 0
+	//loop through rows
+	for _, row := range board_data {
+		x := strings.Split(row, " ")
+		for _, v := range x {
+			fmt.Println("Element is", v, "Count: ", count)
+			if y > 4 {
+				inside = false
+			}
+			if count < 5 && v != " " && inside {
+				number, _ := strconv.Atoi(v)
+				bbd[board_index][y][count] = number
+				count++
+				fmt.Println("ADDED: ", number, "Increased count to: ", count)
+			}
+			if count > 4 && v == "" && y < 5 {
+				y++
+				count = 0
+			}
+		}
+	}
+	//PRINTING
 	//print out all the boards
 	fmt.Println("Boards:")
-	//print all boards
 	for n := 0; n < BOARD_AMOUNT; n++ {
 		for i := range bbd[n] {
 			fmt.Println(bbd[n][i])
@@ -46,20 +64,27 @@ func main() {
 		fmt.Println()
 	}
 
-	//print stuff
-	fmt.Println()
-	fmt.Println(winning_nums, "\n")
+	//we can loop over the winning numbers like this:
+	for _, x := range winning_nums {
+		fmt.Println(x)
+	}
 }
 
-func input_handler(filename string) []string {
+//Returns winning_nums, board_data, board_amount
+func input_handler(filename string) ([]string, []string, int) {
 	f, _ := ioutil.ReadFile(filename)
 	a := string(f)
+
+	//extract the amount of boards
+	newLines := strings.Count(a, "\n")
+	board_amount := (newLines - 1) / 6
 
 	//extract the winning numbers
 	b := strings.Split(a, "\n")
 	numbers := (strings.Split(b[0], ","))
 
+	//
+	board_data := b[2:]
 	//extract the bingo boards somehow and put them in the slice
-
-	return numbers
+	return numbers, board_data, board_amount
 }
