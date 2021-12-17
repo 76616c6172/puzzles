24a61aefc7492a6b58f087d1e22459cba0f83af2
valar <68338647+76616c6172@users.noreply.github.com>
2021-12-16 17:08:32 -0800

    VIP data_wrangler, distinguishes between x1/y1 and x2/y2 pairs correctly for each line

--- adventOfCode2021/day5/vents.go
+++ adventOfCode2021/day5/vents.go
@@ -8,9 +8,10 @@ import (
 	"strings"
 )
 
+// Holds the data for each line segment
 type line struct {
-	start [2]int
-	end   [2]int
+	start [2]int //line.start[x1][y1]
+	end   [2]int //line.end[x2][y2]
 }
 
 // Reads file and returns the contents as a single string with the last newline removed
@@ -25,11 +26,33 @@ func get_input(filename string) string {
 	return string(result)
 }
 
+func data_wrangler(input string) {
+	line_amount := strings.Count(input, "\n") + 1
+	lines := make([]line, line_amount)
+	each_segment := strings.Split(input, "\n")
+
+	// TODO: instead of just printing, store each point coordinate pair correctly in lines[] for each line
+	for i := 0; i < line_amount; i++ {
+		b := strings.Split(each_segment[i], " -> ")
+		fmt.Println(b)
+		for z, v := range b {
+			point := strings.Split(v, ",")
+			if z == 0 {
+				fmt.Println("START:", point[0], point[1])
+			} else {
+				fmt.Println("END:", point[0], point[1])
+
+			}
+		}
+	}
+
+	fmt.Println(lines)
+}
+
 func main() {
 
-	filename := "data"
+	filename := "example"
 	test := get_input(filename)
-
-	fmt.Println(test)
+	data_wrangler(test)
 
 }
